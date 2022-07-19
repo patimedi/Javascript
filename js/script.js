27fff180279168f361d63957c332fcb98892c321
patimedi <pmedinaluvizio@gmail.com>
2022-07-19 20:49:36 -0300

    comentarios

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ let nombre = prompt('Ingresá tu nombre');
 let opciones = prompt('¿Querés suscrbirte para recibir un descuento en tu membresía?: \n1 - Sí \n2 - No')
 let membresiaPromocion = ('Descuento del 10% hasta el 31 de julio');
 
-    
+
 switch (opciones) {
     case '1':
         prompt('ingresa tu mail')
@@ -15,6 +15,7 @@ switch (opciones) {
         const resta = function (precioNormal, descuento) { return 1100 - 110 }
         alert('el precio con descuento es ' + resta(1100 - 110) + ' ¡Gracias!')
         break;
+        // acá me hubiese gustado poner si la compra se realiza antes del 31 de julio entonces el precio es con 10% de descuento pero si es despues de esa fecha el precio es el precio normal
     case '2':
         alert('Gracias por tu respuesta, te esperamos pronto')
         break;
